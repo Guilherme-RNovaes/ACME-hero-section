9ad7eacd51316eaf9e72aa6faffb16c1769d331d
guilh <guilherme00rogerio@gmail.com>
2023-08-29 17:50:39 -0300

    finalizing of the project test

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        primary: '#FF2E63',
+        secondary: '#08D906',
+        dark: '#252A34',
+        light: '#EAEAEA'
+      }
+    },
+    fontFamily: {
+      'sans': ['Montserrat', 'ui-sans-serif', 'system-ui']
+    }
 	},
 	plugins: [],
 }
